fdf912ea7be24245bce926f35e71fc8bb432da88
Mehar Ali <mehar0alii@gmail.com>
2023-11-05 10:21:41 +0500

    responsive footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import {
   FaMusic,
@@ -12,54 +13,56 @@ import {
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white py-8">
-      <div className="container mx-auto p-4 flex flex-wrap justify-center lg:justify-between">
-        <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/5 mb-6">
-          <h2 className="text-xl font-semibold mb-4">Contact Us</h2>
-          <p>123 Main Street</p>
-          <p>City, State ZIP Code</p>
-          <p>Email: example@email.com</p>
-          <p>Phone: (123) 456-7890</p>
+    <footer className="bg-gray-900 text-white py-8 grid place-content-center text-center" >
+      <div className="container mx-auto p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 place-content-center">
+        <div className="mb-6">
+          <h2 className="text-2xl font-semibold mb-4">Contact Us</h2>
+          <address className='text-center md:text-start space-y-3 '>
+            <p>123 Main Street</p>
+            <p>City, State ZIP Code</p>
+            <p>Email: example@email.com</p>
+            <p>Phone: (123) 456-7890</p>
+          </address>
         </div>
 
-        <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/5 mb-6">
-          <h2 className="text-xl font-semibold mb-4">Categories</h2>
-          <ul>
-            <li>
-              <FaMusic className="text-lg inline mr-2" />
+        <div className="mb-6">
+          <h2 className="text-2xl font-semibold mb-4">Categories</h2>
+          <ul className='grid place-content-center space-y-3'>
+            <li className="flex items-center mb-2 cursor-pointer hover:text-rose-500">
+              <FaMusic className="text-2xl mr-2" />
               Music
             </li>
-            <li>
-              <FaHeadphones className="text-lg inline mr-2" />
+            <li className="flex items-center mb-2 cursor-pointer hover:text-rose-500">
+              <FaHeadphones className="text-2xl mr-2" />
               Headphones
             </li>
-            <li>
-              <FaMicrophone className="text-lg inline mr-2" />
+            <li className="flex items-center mb-2 cursor-pointer hover:text-rose-500">
+              <FaMicrophone className="text-2xl mr-2" />
               Microphones
             </li>
-            <li>
-              <FaGuitar className="text-lg inline mr-2" />
+            <li className="flex items-center mb-2 cursor-pointer hover:text-rose-500">
+              <FaGuitar className="text-2xl mr-2" />
               Guitars
             </li>
-            <li>
-              <FaDrum className="text-lg inline mr-2" />
+            <li className="flex items-center mb-2 cursor-pointer hover:text-rose-500">
+              <FaDrum className="text-2xl mr-2" />
               Drums
             </li>
           </ul>
         </div>
 
-        <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/5 mb-6">
-          <h2 className="text-xl font-semibold mb-4">Social Media</h2>
-          <div className="flex space-x-4">
-            <a href="#">
-              <FaFacebook className="text-2xl" />
-            </a>
-            <a href="#">
-              <FaTwitter className="text-2xl" />
-            </a>
-            <a href="#">
-              <FaInstagram className="text-2xl" />
-            </a>
+        <div className="mb-6">
+          <h2 className="text-2xl font-semibold mb-4">Social Media</h2>
+          <div className="flex justify-center items-center flex-row space-x-4">
+            <Link href="#" className="text-3xl hover:text-rose-400">
+              <FaFacebook />
+            </Link>
+            <Link href="#" className="text-3xl hover:text-rose-400">
+              <FaTwitter />
+            </Link>
+            <Link href="#" className="text-3xl hover:text-rose-400">
+              <FaInstagram />
+            </Link>
           </div>
         </div>
       </div>
